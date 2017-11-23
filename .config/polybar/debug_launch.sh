dae0756adc2e8cc75b0a60a88f1aac22ed07c0bb
ianchrist <iechrist14@gmail.com>
2017-11-22 20:59:37 -0600

    Updated launch script

---
+++ .config/polybar/debug_launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+echo "Killing old polybar instances..."
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+echo "Launching polybar..."
+polybar main &
+polybar tray &
+echo "Polybar launched"
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-echo "Killing old polybar instances..."
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-echo "Launching polybar..."
+# Launch bar1 and bar2
 polybar main &
 polybar tray &
-echo "Polybar launched"
+
+echo "Bars launched..."
